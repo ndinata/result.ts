d0407e2c5246c5d7f7b70266ac55c03d32678631
Nico Dinata <nicodnt@icloud.com>
2023-07-15 20:03:20 +0700

    Implement more `Result` methods

--- types/result.test.ts
+++ types/result.test.ts
@@ -21,17 +21,21 @@ test("`unwrapOr` works", () => {
 test("`isOk` works", () => {
   let x = new Ok(-3);
   expect(x.isOk()).toBeTruthy();
+  expect(x.ok()).toBe(-3);
 
   let y = new Err("error");
   expect(y.isOk()).toBeFalsy();
+  expect(y.ok()).toBe(undefined);
 });
 
 test("`isErr` works", () => {
   let x = new Ok(-3);
   expect(x.isErr()).toBeFalsy();
+  expect(x.err()).toBe(undefined);
 
   let y = new Err("error");
   expect(y.isErr()).toBeTruthy();
+  expect(y.err()).toBe('error');
 });
 
 test("`map` works", () => {
@@ -73,6 +77,31 @@ test("`match` works", () => {
   expect(y.match(ok, err)).toBe(0);
 });
 
+test("`and` works", () => {
+  let a: Result<number, string> = new Ok(2);
+  let b: Result<number, string> = new Err("err");
+  expect(a.and(b)).toEqual(new Err("err"));
+  expect(b.and(a)).toEqual(new Err("err"));
+
+  let c = new Err("some");
+  let d = new Err("error");
+  expect(c.and(d)).toEqual(new Err("some"));
+
+  let e = new Ok(2);
+  let f = new Ok(100);
+  expect(e.and(f)).toEqual(new Ok(100));
+});
+
+test("`andThen` works", () => {
+  const sqThenToString = (x: number): Result<string, string> =>
+    new Ok((x * x).toString());
+
+  expect(new Ok(2).andThen(sqThenToString)).toEqual(new Ok("4"));
+  expect(new Err<number, string>("nan").andThen(sqThenToString)).toEqual(
+    new Err("nan")
+  );
+});
+
 test("`or` works", () => {
   let a = new Ok(2);
   let b: Result<number, string> = new Err("err");
@@ -86,4 +115,49 @@ test("`or` works", () => {
   let e = new Ok(2);
   let f = new Ok(100);
   expect(e.or(f)).toEqual(new Ok(2));
+});
+
+test("`orElse` works", () => {
+  const sq = (x: number): Result<number, number> => new Ok(x * x);
+  const err = (x: number): Result<number, number> => new Err(x);
+
+  expect(new Ok<number, number>(2).orElse(sq).orElse(sq)).toEqual(new Ok(2));
+  expect(new Ok<number, number>(2).orElse(err).orElse(sq)).toEqual(new Ok(2));
+  expect(new Err<number, number>(3).orElse(sq).orElse(err)).toEqual(new Ok(9));
+  expect(new Err<number, number>(3).orElse(err).orElse(err)).toEqual(
+    new Err(3)
+  );
+});
+
+test("`wrap` works", () => {
+  let unthrowing = () => 100;
+  expect(Result.wrap(unthrowing)).toEqual(new Ok(100));
+
+  let throwing = (input: number) => {
+    if (input >= 0) {
+      return input;
+    }
+    throw new Error("err");
+  };
+  expect(Result.wrap(() => throwing(1))).toEqual(new Ok(1));
+  expect(Result.wrap(() => throwing(-1))).toEqual(new Err(new Error("err")));
+  expect(Result.wrap(() => throwing(-1), new Error("foo"))).toEqual(
+    new Err(new Error("foo"))
+  );
+});
+
+test("`asyncWrap` works", async () => {
+  const sleep = () => new Promise((r) => setTimeout(r, 1000));
+  let asyncThrowing = async (input: number) => {
+    await sleep();
+    if (input >= 0) {
+      return 42;
+    }
+    throw new Error("err");
+  };
+
+  expect(Result.asyncWrap(() => asyncThrowing(1))).resolves.toEqual(new Ok(42));
+  expect(Result.asyncWrap(() => asyncThrowing(-1))).resolves.toEqual(
+    new Err(new Error("err"))
+  );
 });
--- types/result.ts
+++ types/result.ts
@@ -1,6 +1,12 @@
 /** Similar to Rust's `Result` type: either an `Ok` or `Err` variant. */
 export type Result<T, E> = Ok<T, E> | Err<T, E>;
 
+/** Alias for a `Result` inside a `Promise`. */
+export type AsyncResult<T, E> = Promise<Result<T, E>>;
+
+/** Alias for `T` | `undefined`. */
+export type Option<T> = T | undefined;
+
 /** Collection of methods that all variants of `Result` should implement. */
 interface IResult<T, E> {
   /** Returns the inner `Ok` value or the provided default. */
@@ -9,8 +15,14 @@ interface IResult<T, E> {
   /** Returns whether this `Result` is an `Ok` variant. */
   isOk(): this is Ok<T, E>;
 
+  /** Returns the inner `Ok` value if is `Ok`; returns `undefined` otherwise. */
+  ok(): Option<T>;
+
   /** Returns whether this `Result` is an `Err` variant. */
   isErr(): this is Err<T, E>;
+
+  /** Returns the inner `Err` value if is `Err`; returns `undefined` otherwise. */
+  err(): Option<E>;
 
   /**
    * Applies the supplied function to the inner value if it's an `Ok` variant;
@@ -35,8 +47,17 @@ interface IResult<T, E> {
    */
   match<U>(ok: (t: T) => U, err: (e: E) => U): U;
 
-  /** Returns `res` if self is an `Err`; otherwise, returns self's `Ok` value. */
+  /** Returns `res` if self is an `Ok`; otherwise, returns self's `Err`. */
+  and<U>(res: Result<U, E>): Result<U, E>;
+
+  /** Calls `op` if self is an `Ok`; otherwise, returns self's `Err`. */
+  andThen<U>(op: (t: T) => Result<U, E>): Result<U, E>;
+
+  /** Returns `res` if self is an `Err`; otherwise, returns self's `Ok`. */
   or<F>(res: Result<T, F>): Result<T, F>;
+
+  /** Calls `op` if self is an `Err`; otherwise, returns self's `Ok`. */
+  orElse<F>(op: (e: E) => Result<T, F>): Result<T, F>;
 }
 
 /** The `Ok` variant of `Result`. */
@@ -59,8 +80,16 @@ export class Ok<T, E> implements IResult<T, E> {
     return true;
   }
 
+  ok(): Option<T> {
+    return this.t;
+  }
+
   isErr(): this is Err<T, E> {
     return false;
+  }
+
+  err(): Option<E> {
+    return undefined;
   }
 
   map<U>(f: (t: T) => U): Result<U, E> {
@@ -79,7 +108,19 @@ export class Ok<T, E> implements IResult<T, E> {
     return ok(this.t);
   }
 
+  and<U>(res: Result<U, E>): Result<U, E> {
+    return res;
+  }
+
+  andThen<U>(op: (t: T) => Result<U, E>): Result<U, E> {
+    return op(this.t);
+  }
+
   or<F>(_: Result<T, F>): Result<T, F> {
+    return new Ok(this.t);
+  }
+
+  orElse<F>(_: (e: E) => Result<T, F>): Result<T, F> {
     return new Ok(this.t);
   }
 }
@@ -104,8 +145,16 @@ export class Err<T, E> implements IResult<T, E> {
     return false;
   }
 
+  ok(): Option<T> {
+    return undefined;
+  }
+
   isErr(): this is Err<T, E> {
     return true;
+  }
+
+  err(): Option<E> {
+    return this.e;
   }
 
   map<U>(_: (t: T) => U): Result<U, E> {
@@ -124,7 +173,91 @@ export class Err<T, E> implements IResult<T, E> {
     return err(this.e);
   }
 
+  and<U>(_: Result<U, E>): Result<U, E> {
+    return new Err(this.e);
+  }
+
+  andThen<U>(_: (t: T) => Result<U, E>): Result<U, E> {
+    return new Err(this.e);
+  }
+
   or<F>(res: Result<T, F>): Result<T, F> {
     return res;
+  }
+
+  orElse<F>(op: (e: E) => Result<T, F>): Result<T, F> {
+    return op(this.e);
+  }
+}
+
+export namespace Result {
+  // Wrapping a throwing function (e.g. from a 3rd party lib) into returning a
+  // `Result` is a great idea which I encountered in another similar repo:
+  // https://github.com/supermacro/neverthrow/
+  // The concept for `wrap()` and `asyncWrap()` below is inspired by that repo's
+  // `fromThrowable()`, although the usage semantics ultimately differ.
+
+  /**
+   * Wraps a throwing function `f` in a `Result`.
+   *
+   * If `f` throws when called, returns an `Err` containing the thrown error (or
+   * `e` if provided). Otherwise, returns an `Ok` containing the return value of `f`.
+   *
+   * NOTE: `f` cannot be async (returns a Promise). For that, use `asyncWrap()` instead.
+   *
+   * Example:
+   * ```typescript
+   * const someThrowingFunc = (i: number) => {
+   *   if (i > 0) {
+   *     return 42;
+   *   }
+   *   throw new Error("err");
+   * };
+   * let x = Result.wrap(() => someThrowingFunc(2));
+   * let y = x.unwrapOr(0)); // y == 42
+   * ```
+   */
+  export function wrap<T, E = Error>(
+    f: T extends Promise<any> ? never : (...args: any[]) => T,
+    e?: E
+  ): Result<T, E> {
+    try {
+      return new Ok(f());
+    } catch (err: any) {
+      return new Err(e || err);
+    }
+  }
+
+  /**
+   * Wraps a throwing async function `f` in an `AsyncResult`.
+   *
+   * If `f` throws when called, returns a Promise-wrapped `Err` containing the
+   * thrown error (or `e` if provided). Otherwise, returns a Promise-wrapped `Ok`
+   * containing the return value of `f`.
+   *
+   * NOTE: `f` has to be async (returns a Promise). For sync functions, use
+   * `wrap()` instead.
+   *
+   * Example:
+   * ```typescript
+   * const asyncThrowingFunc = async (i: number) => {
+   *   if (i > 0) {
+   *     return 42;
+   *   }
+   *   throw new Error("err");
+   * };
+   * let x = await Result.asyncWrap(() => asyncThrowingFunc(2));
+   * let y = x.unwrapOr(0); // y == 42;
+   * ```
+   */
+  export async function asyncWrap<T, E = Error>(
+    f: (...args: any[]) => Promise<T>,
+    e?: E
+  ): AsyncResult<T, E> {
+    try {
+      return new Ok(await f());
+    } catch (err: any) {
+      return new Err(e || err);
+    }
   }
 }
